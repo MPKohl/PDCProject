9a285d060476b8a891095d911c2e7943dcd363de
ShanonJackson <phazeblade@hotmail.com>
2015-08-06 14:50:13 +1200

    Added Controller class skeletons.

---
+++ PDC Project/src/pdc/project/Controller/DrawController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdc.project.Controller;
+
+/**
+ *
+ * @author shanon
+ */
+public class DrawController {
+//values
+
+//construcotrs
+public DrawController() {
+
+}
+//methods
+ //getters and setters
+}
