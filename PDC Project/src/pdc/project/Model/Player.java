c0becffbbfb0c17689e3dd57b3279bad6ce11b8e
shanon <shanon@shanon-pc>
2015-08-11 20:10:44 +1200

    Player/Warrior/Archer/Wizard Classes Changed.

--- PDC Project/src/pdc/project/Model/Player.java
+++ PDC Project/src/pdc/project/Model/Player.java
@@ -5,21 +5,95 @@
  */
 package pdc.project.Model;
 
+import java.util.HashMap;
+
 /**
  *
  * @author MPKohl
  */
-public class Player {
-    //Name
-    //Class
-    //Health
-    //Exp
-    //Inventory - Array
-    //Score
-    //Equipped - HashMap
-    //Chance to hit     
-    //Chance to crit
-    //Chance to dodge
+public abstract class Player {
+    private String name = "";
+    private int health;
+    private int exp;
+    private Item[] inventory;
+    private int score;
+    private HashMap equipped = new HashMap();
+    private double hitChance;
+    private double critChance;
+    private double dodgeChance;
+    //Construcotrs.
+    public Player() {}
+    public Player(String name, int health, int exp, Item[] inventory,
+            int score, HashMap equipped, double hitChance, double critChance, 
+            double dodgeChance) {
+        this.name = name;
+        this.health = health;
+        this.exp = exp;
+        this.inventory = inventory;
+        this.score = score;
+        this.equipped = equipped;
+        this.hitChance = hitChance;
+        this.critChance = critChance;
+        this.dodgeChance = dodgeChance;     
+    }        
     
-    //getClass()
+    //Methods.
+    public abstract String findClass();   //illegal to call getClass.
+    
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+    public int getHealth() {
+        return health;
+    }
+    public int getExp() {
+        return exp;
+    }
+    public Item[] inventory() {
+        return inventory;  //will change to properally return.
+    }
+    public int getScore() {
+        return score;
+    }
+    public HashMap getEquipped() {
+        return equipped;
+    }
+    public double getHitChance() {
+        return hitChance;
+    }
+    public double getCritChance() {
+        return critChance;
+    }
+    public double getDodgeChance() {
+        return dodgeChance;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+    public void setInventory(Item[] inventory) {
+        this.inventory = inventory;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void setEquipped(HashMap equipped) {
+        this.equipped = equipped;
+    }
+    public void setCritChance(double critChance) {
+        this.critChance = critChance;
+    }
+    public void setHitChance(double hitChance) {
+        this.hitChance = hitChance;
+    }
+    public void setDodgeChance(double dodgeChance) {
+        this.dodgeChance = dodgeChance;
+    }
+ 
 }
