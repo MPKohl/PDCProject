2b6f9a5393f5f8c54b163fd1f7701500eef09286
shanon <shanon@shanon-pc>
2015-08-14 11:06:10 +1200

    Player/Warrior/Archer/Wizard Classes Changed.

--- PDC Project/src/pdc/project/Model/Archer.java
+++ PDC Project/src/pdc/project/Model/Archer.java
@@ -5,11 +5,26 @@
  */
 package pdc.project.Model;
 
+import java.util.HashMap;
+
 /**
  *
  * @author MPKohl
  */
-public class Archer {
+public class Archer extends Player {
+    public boolean extraAttack = false; //will be used to determine if extra attack is granted.
+        public Archer(String name, int health, int exp, Item[] inventory,
+            int score, HashMap equipped, double hitChance, double critChance, 
+            double dodgeChance) {
+            
+        super(name,health,exp,inventory,score,equipped,hitChance,critChance,dodgeChance);
+        critChance = 20;
+        extraAttack = true;
+        health = 80;
+    } 
+    public String findClass() {
+        return "Archer";
+    }
     //Extra attack at beginning of combat
     //Higher crit damage
     //Low health
