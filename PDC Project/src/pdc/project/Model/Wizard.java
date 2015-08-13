2e1a0eb19d0aa1fffe6df1022d140490eba93997
shanon <shanon@shanon-pc>
2015-08-14 11:05:52 +1200

    Player/Warrior/Archer/Wizard Classes Changed.

--- PDC Project/src/pdc/project/Model/Wizard.java
+++ PDC Project/src/pdc/project/Model/Wizard.java
@@ -5,12 +5,28 @@
  */
 package pdc.project.Model;
 
+import java.util.HashMap;
+
 /**
  *
  * @author MPKohl
  */
-public class Wizard {
-    //Spell list
-    //Three chosen spells
-    //Reduced health
+public class Wizard extends Player {
+    public String[] spells;    //Actually probably need a 'spell' class.
+    public Wizard(String name, int health, int exp, Item[] inventory,
+            int score, HashMap equipped, double hitChance, double critChance, 
+            double dodgeChance) {
+            
+        super(name,health,exp,inventory,score,equipped,hitChance,critChance,dodgeChance);
+        health = 80;
+        setSpells(); // to be changed later.
+    }
+    public String findClass() {
+        return "Wizard";
+    }
+    public void setSpells() {
+        String spellList[] = {"Fireball", "Ignite", "Shroud"};
+        spells = spellList;
+    }
+
 }
