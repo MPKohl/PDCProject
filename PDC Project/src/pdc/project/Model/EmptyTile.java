a7f07733e506e24ad0962d7284f40fe33a9a0c44
MPKohl <MPKohl@MPKOHL-LAPTOP>
2015-08-22 16:45:41 +1200

    Created empty tiles and made a new fancy gameboard!

---
+++ PDC Project/src/pdc/project/Model/EmptyTile.java
@@ -0,0 +1,10 @@
+package pdc.project.Model;
+
+/**
+ * Class for empty tiles. Extends Tile.
+ */
+public class EmptyTile extends Tile{
+    public EmptyTile(){
+        setType(TileType.EMPTY);
+    }
+}
